66231ad21c09499f87ddff76f5eb960b6d94feec
sthammer <sthummer0@gmail.com>
2021-02-24 23:50:04 +0600

    11

--- js_3.js
+++ js_3.js
@@ -1,12 +1,11 @@
 /* Дана строка. Необходимо вывести в консоль перевёрнутый вариант. 
 Например, "Hello" -> "olleH" */
 
-function reverseString(res) {
-    let reversed = res.split("");
-    let reversedArray = reversed.reverse();
-    let joinedArray = reversedArray.join("")
-    return joinedArray;
-}
 
-const reversedResult=prompt("Enter string")
-console.log(reverseString(reversedResult))
+let str = prompt ('Введите слово или фразу');
+
+let reverse = str.split('').reverse().join('');
+
+console.log('Оригинальная строка ' + str)
+
+console.log('Перевернутая строка ' + reverse)
--- js_4.js
+++ js_4.js
@@ -1,6 +1,15 @@
 /* Записать в переменную случайное целое число в диапазоне [0; 100]. 
    Используйте объект Math. */
 
-   var random_x = (Math.floor(Math.random() * Math.floor(100)));
-
-   console.log(random_x)
+   function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+  }
+  
+  // или
+  console.log(getRandomIntInclusive(0, 100))
+  
+  var random_x = (Math.floor(Math.random() * Math.floor(100)));
+  
+     console.log(random_x)
